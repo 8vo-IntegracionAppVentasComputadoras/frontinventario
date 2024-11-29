e74784c06c46a4d0688f2e1fdaa6c2c003651b80
Bryan Steven Salvatierra Quesada <pogba2139657@gmail.com>
2024-11-29 05:34:45 -0500

    tre

--- src/app/config/helper.ts
+++ src/app/config/helper.ts
@@ -1,2 +1,6 @@
-let baserUrl = 'http://localhost:8080'
+//let baserUrl = 'https://back-inventario.fly.dev'
+let baserUrl = 'https://back-inventario-long-morning-710.fly.dev'
 export default baserUrl;
+//dsdsdsdsdsdsdsds
+//dssvc  sads a c ads cv ds bv b
+
